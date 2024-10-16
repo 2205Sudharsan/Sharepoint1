a43a4b1882340dcf0603e6a2849ca47db9b18fd2
devaraj106 <www.devaraj8224215@gmail.com>
2024-10-16 13:22:04 +0530

    ssdsd

---
+++ src/webparts/sharepointtest1/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISharepointtest1WebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'Sharepointtest1WebPartStrings' {
+  const strings: ISharepointtest1WebPartStrings;
+  export = strings;
+}
